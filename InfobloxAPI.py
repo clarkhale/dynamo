352f740c6fae5e6a381e4c60546249dd6cd11bc6
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2013-11-06 11:03:35 -0500

    Rewrote dynamo using InfobloxAPI.  Renamed InfoblockAPI.py.

--- InfobloxAPI.py
+++ InfobloxAPI.py
@@ -1,14 +1,44 @@
+"""
+A class object to abstract interaction with the Infoblox Grid Manager
+Web API v1.1.
+
+API v1.1 info:
+Docs: https://support.infoblox.com/technical-documents
+Spec: Infoblox_RESTful_API_Documentation_1.1                          
+
+"""
+
 import json
 import os
 import requests
 
-class InfoBloxAPI:
-    """Object holding data, methods to interface with InfoBlox Grid Manager
-       API 1.1 spec.
+class InfobloxAPI:
+    """ Return an object with all the information required to start
+        making WAPI calls.
+
+        Examples:
+
+        Generate a new hostname and assign it to the first available
+        IP address in a network.  Start checking availble names from
+        prexix0010.example.com:
+        
+        from InfobloxAPI import InfobloxAPI
+
+        i = InfobloxAPI('gridmgr.exapmle.com', 'username', 'password')
+        network = '192.168.1.0/24'
+        i.rh_add( network, 
+                  i.next_available_ip(network),
+                  i.next_available_name('prefix', 10, 'example.com') )
+    
     """
 
     def __init__(self, server, username, password, 
                  verbose=False, debug=False, verify=True):
+        """ Initialize InfobloxAPI.  Requires API server to connect to,
+            as well as username and password for authentication and
+            authorization.  No calls are made yet, so there is no test
+            yet of the information the object is initialized with.
+        """
         self.server   = server
         self.username = username
         self.password = password
@@ -19,9 +49,13 @@ class InfoBloxAPI:
 
         self.url      = 'https://' + self.server + '/wapi/v1.1/'
 
-    def rh_exists(self, hostname):
-        """record:host for name=hostname exist?"""
-        data = { 'name'  : hostname  }
+    def rh_exists(self, hostname, like=False):
+        """Test if a record:host for name=hostname exists.
+        """
+        if like:
+            data = { 'name~' : hostname  }
+        else:
+            data = { 'name'  : hostname  }
         self.r = requests.get(self.url + 'record:host',
                               params=data,
                               auth=(self.username, self.password),
@@ -31,23 +65,26 @@ class InfoBloxAPI:
             print self.r.text
         return self.r.json()
 
-    def rh_exists_like(self, hostname, max=10):
-        """record:host for name~=hostname exist?"""
-        data = { 'name~'        : hostname,
-                 '_max_results' : max
-                 }
-        self.r = requests.get(self.url + 'record:host',
-                              params=data,
-                              auth=(self.username, self.password),
-                              verify=self.verify)
-        if self.debug:
-            print "inside rh_exists_like:"
-            print self.r.text
-        return self.r.json()
+    # def rh_exists_like(self, hostname, max=10):
+    #     """record:host for name~=hostname exist?"""
+    #     data = { 'name~'        : hostname,
+    #              '_max_results' : max
+    #              }
+    #     self.r = requests.get(self.url + 'record:host',
+    #                           params=data,
+    #                           auth=(self.username, self.password),
+    #                           verify=self.verify)
+    #     if self.debug:
+    #         print "inside rh_exists_like:"
+    #         print self.r.text
+    #     return self.r.json()
 
-    def ra_exists(self, hostname):
+    def ra_exists(self, hostname, like=False):
         """record:a_record for name=hostname exist?"""
-        data = { 'name'  : hostname  }
+        if like:
+            data = { 'name~' : hostname  }
+        else:
+            data = { 'name'  : hostname  }
         self.r = requests.get(self.url + 'record:a',
                               params=data,
                               auth=(self.username, self.password),
@@ -57,19 +94,19 @@ class InfoBloxAPI:
             print self.r.text
         return self.r.json()
 
-    def ra_exists_like(self, hostname, max=10):
-        """record:a_record for name~=hostname exist?"""
-        data = { 'name~'        : hostname,
-                 '_max_results' : max
-                 }
-        self.r = requests.get(self.url + 'record:a',
-                              params=data,
-                              auth=(self.username, self.password),
-                              verify=self.verify)
-        if self.debug:
-            print "inside ra_exists_like:"
-            print self.r.text
-        return self.r.json()
+    # def ra_exists_like(self, hostname, max=10):
+    #     """record:a_record for name~=hostname exist?"""
+    #     data = { 'name~'        : hostname,
+    #              '_max_results' : max
+    #              }
+    #     self.r = requests.get(self.url + 'record:a',
+    #                           params=data,
+    #                           auth=(self.username, self.password),
+    #                           verify=self.verify)
+    #     if self.debug:
+    #         print "inside ra_exists_like:"
+    #         print self.r.text
+    #     return self.r.json()
 
     def network_ref(self, network):
         """Return _ref where network=network"""
@@ -85,7 +122,15 @@ class InfoBloxAPI:
         return self.r.json()[0][u'_ref']
 
     def next_available_ip(self, network, count=10):
-        """Return next available IP in network"""
+        """Return next available IP in network
+
+           Look up the network reference, then get the count next
+           available IPs.  Sequentially test each one via ping, and
+           return the first one that does not respond.
+
+           In verbose mode, it throws out a warning if one of the
+           available IPs responds to ping.
+        """
         netref = self.network_ref(network)
         data = { '_function' : 'next_available_ip', 
                  'num'       : count }
@@ -126,6 +171,7 @@ class InfoBloxAPI:
         data = { 
             'ipv4addrs'  : [ { 'ipv4addr' : ipaddr }, ],
             'name'       : fqdn,
+            # removed hardcoded view and comment.  Add date to comment. TODO
             'view'       : 'Infoblox Internal',
             'comment'    : 'Dynamically entered by ' + self.username
             }
@@ -138,9 +184,21 @@ class InfoBloxAPI:
         if self.debug:
             print "inside rh_add: " + fqdn
             print self.r.text
+            
+        return( { 'ipaddr' : ipaddr,
+                  'name'   : fqdn } )
 
-    def next_available_name(self, prefix, domain, cnt_start, digits=4, run=1024):
-        """Find the next available name of form prefix + 0001 + domain"""
+
+    def next_available_name(self, prefix, cnt_start, domain, digits=4, run=1024):
+        """Find the next available name of form prefix + 0001 + domain
+
+           Starts at cnt_start and then checks sequentially up to 
+           cnt_start + run, looking for available names.
+
+           Checks the Infoblox API for record:host entry, record:a
+           entry, and finally just a DNS lookup.  The first name which
+           shows clear in all those is returned.
+        """
         for i in range(cnt_start, run):
             if self.debug:
                 print "i: " + str(i)
--- dynamo.py
+++ dynamo.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 
-import base64
 import getpass
+from InfobloxAPI import InfobloxAPI
 import json
 from optparse import OptionParser
 import os
 import re
 import sys
-import urllib
-import urllib2
-from urlparse import urlparse
 
 debug   = False
 verbose = False
 
-baseurl = 'https://10.69.15.170/wapi/v1.1/'
+gridmgr = '10.69.15.170'
 
+# Remove hardcode
 allowed_ranges  = [ '10.39.2.0/23',
                     ]
 
+# Remove hardcode
 allowed_domains = [ 'ci.snops.net',
                     ]
 
@@ -124,19 +123,24 @@ def main():
     if debug:
         print "{0:s}, {1:s}".format(uid, passwd)
 
-    iprangeref = get_network_ref(uid, passwd, iprange)
-    availip = get_next_available(uid, passwd, iprangeref, 5)
-    if verbose:
-        print "Next available IP address is " + availip
+    wapi = InfobloxAPI(gridmgr, uid, passwd, 
+                       debug=True, verbose=True, verify=False)
 
+    r = { }
     if not noaction:
-        hostref = register_host(uid, passwd, availip, hostname)
+        r = wapi.rh_add( iprange,
+                         wapi.next_available_ip(iprange),
+                         hostname )
 
+    if not r:
+        print "Unknown error in WAPI call."
+        return 0
+    
     if outputtype.lower() == "user":
-        print "Assigning " + availip + " to " + hostname + "."
+        print "Assigning " + r['ipaddr'] + " to " + r['name'] + "."
     elif outputtype.lower() == "json":
-        print json.dumps( { 'hostname'  : hostname,
-                            'ipaddress' : availip,
+        print json.dumps( { 'hostname'  : r['name'],
+                            'ipaddress' : r['ipaddr'],
                             'iprange'   : iprange } )
     elif outputtype.lower() == "xml":
         print "Sorry, XML not supported yet..."
@@ -170,177 +174,6 @@ def get_login(uid, passwd):
 
     return (uid, passwd)
 
-
-def get_network_ref(uid, passwd, iprange):
-    """Get and return the Infoblox refencence to this network.
-       Much of this taken from http://www.voidspace.org.uk/python/articles/authentication.shtml#introduction
-    """
-
-    if debug:
-        print "Entering get_network_ref: {0:s}, {1:s}, {2:s}".format(uid, 
-                                                                     passwd, 
-                                                                     iprange)
-
-    url    = baseurl + "network"
-    values = { 'network' : iprange }
-    data   = urllib.urlencode(values)
-    if debug:
-        print "Trying URL {0:s}".format(url + '?' + data)
-
-    req    = urllib2.Request(url + '?' + data)
-    handle = rest_call(uid, passwd, req)
-
-
-
-    #results = handle.read()
-
-    data = json.load(handle)
-
-    if len(data) > 1:
-        print "I should have only one network returned, erroring out."
-        sys.exit(1)
-
-    if debug:
-        print "Full data returned:"
-        print data
-    if verbose:
-        print "{0:s} ref: {1:s}".format(iprange,data[0]["_ref"])
-
-    return data[0]["_ref"]
-
-def get_next_available(uid, passwd, iprangeref, cnt=10):
-    url    = baseurl + iprangeref
-    
-    values = { '_function' : 'next_available_ip',
-               'num'       : cnt
-               }
-    data = urllib.urlencode(values)
-
-    if debug:
-        print "next avail url: {0:s}".format(url)
-
-    req    = urllib2.Request(url, data)
-    if debug:
-        print req.get_full_url()
-        print req.get_data()
-        print req.get_method()
-        # help(req)
-    handle = rest_call(uid, passwd, req)
-
-    #results = handle.read()
-
-    data = json.load(handle)
-    if debug:
-        print data
-
-    if len(data) > 1:
-        print "I should have only one network returned, erroring out."
-        sys.exit(1)
-
-    for ipaddr in data['ips']:
-        resp = os.system("ping -c 1 " + ipaddr + " >> /dev/null")
-        if resp == 0:
-            if debug:
-                print ipaddr, "is up!"
-            if verbose:
-                print "\n"
-                print "========================================================"
-                print "Please open a case with the OCC.  IP {0:s}".format(ipaddr)
-                print "is pingable, but is not listed in Infoblox Grid as a"
-                print "used IP address. (ignore for now)" 
-                print "========================================================"
-                print "\n"
-        else:
-            if debug:
-                print ipaddr, "is down!"
-            return ipaddr
-    print "No IP addresses available, exiting."
-    sys.exit(1)
-
-def register_host(uid, passwd, ipaddr, fqdn):
-    url    = baseurl + "record:host"
-    values = json.dumps( { 'ipv4addrs' : [ { 'ipv4addr' : ipaddr } ],
-                           'name'      : fqdn,
-                           'view'      : 'Infoblox Internal'
-                           })
-
-    # data = urllib.urlencode(values)
-
-    if debug:
-        print "register host url: {0:s}".format(url)
-
-    req    = urllib2.Request(url, values, {'Content-Type': 'application/json'})
-    if debug:
-        print req.get_full_url()
-        print req.get_data()
-        print req.get_method()
-        # help(req)
-    handle = rest_call(uid, passwd, req)
-
-    data = json.load(handle)
-
-    if debug:
-        print "Full data returned:"
-        print data
-    if verbose:
-        print "{0:s} ref: {1:s}".format(fqdn,data)
-
-    return data
-
-
-def rest_call(uid, passwd, urllib2req):
-    try:
-        resp   = urllib2.urlopen(urllib2req)
-    except IOError, e:
-        pass
-    else:
-        print "Page not protected?  That shouldn't happen; erroring out..."
-        sys.exit(1)
-
-    if not hasattr(e, 'code') or e.code != 401:
-        # we got an error - but not a 401 error
-        print "This page isn't protected by authentication."
-        print 'But we failed for another reason.'
-        sys.exit(1)
-
-    authline = e.headers['www-authenticate']
-    # this gets the www-authenticate line from the headers
-    # which has the authentication scheme and realm in it
-
-
-    authobj = re.compile(
-        r'''(?:\s*www-authenticate\s*:)?\s*(\w*)\s+realm=['"]([^'"]+)['"]''',
-        re.IGNORECASE)
-    # this regular expression is used to extract scheme and realm
-    matchobj = authobj.match(authline)
-
-    if not matchobj:
-        # if the authline isn't matched by the regular expression
-        # then something is wrong
-        print 'The authentication header is badly formed.'
-        print authline
-        sys.exit(1)
-
-    scheme = matchobj.group(1)
-    realm = matchobj.group(2)
-    # here we've extracted the scheme
-    # and the realm from the header
-    if scheme.lower() != 'basic':
-        print 'This example only works with BASIC authentication.'
-        sys.exit(1)
-
-    base64string = base64.encodestring(
-        '%s:%s' % (uid, passwd))[:-1]
-    authheader =  "Basic %s" % base64string
-    urllib2req.add_header("Authorization", authheader)
-    try:
-        handle = urllib2.urlopen(urllib2req)
-    except IOError, e:
-        # here we shouldn't fail if the username/password is right
-        print "It looks like the username or password is wrong."
-        sys.exit(1)
-
-    return handle
 
 
 if __name__ == "__main__":
